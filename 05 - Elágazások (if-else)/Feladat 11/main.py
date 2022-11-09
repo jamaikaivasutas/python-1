207f085f1440fac28c7e4c51a74cd28da731d4cc
dunamik <papp.dani2206@gmail.com>
2022-11-09 07:55:33 +0100

    202211092

--- 05 - Elágazások (if-else)/Feladat 11/main.py
+++ 05 - Elágazások (if-else)/Feladat 11/main.py
@@ -10,14 +10,10 @@ number=int(input())
 system('cls')
 
 if(number > 0):
-    if(number % 2 == 0):
-        if(number % 5 == 0):
-            print("A szám páros, pozitív, 5-el osztható.")
-        else:
-            print("A szám páros, pozitív, nem osztható 5-el.")
-elif(number % 3 == 0):
+    print("A szám negatív.")
+if(number % 3 == 0):
     print(f"BAZ")
-elif(number % 2 == 0):
+if(number % 2 == 0):
     print(f"BIZ")
 else:
     print(f"A szám nem osztható se 2-vel,3-al se.")
