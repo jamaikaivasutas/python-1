3740478af1f18d9c816d8f64c277c30cf6a7912d
dunamik <papp.dani2206@gmail.com>
2023-02-02 13:28:52 +0100

    while3

--- 08 - While Ciklus/Feladat 06/main.py
+++ 08 - While Ciklus/Feladat 06/main.py
@@ -8,3 +8,11 @@ while(age >= 99 or age <= 0):
     if(age >= 99 or age <= 0):
         print("Nem jó, adjon meg másikat!")
 
+if(age <= 6):
+    print("gyerek")
+elif(age <= 18):
+    print("iskolás")
+elif(age <= 65):
+    print("dolgozó")
+else:
+    print("nyugdíjas")
