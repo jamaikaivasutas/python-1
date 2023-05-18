42df02ce7eb04c1322692d30a200be126f56e9ee
dunamik <papp.dani2206@gmail.com>
2023-05-18 08:45:57 +0200

    elso dupla ora

---
+++ 12 - Szöveges állomanyok feldolgozása/Feladat 01/fileIO/studentIO.py
@@ -0,0 +1,32 @@
+from typing import *
+from io import open
+import os
+from model.student import Student
+fileName:str = "data/adatok.txt"
+basepath: str = os.path.dirname(os.path.abspath(__file__))
+fileFullPath: str = os.path.join(basepath, fileName)
+
+oneLine:str = None
+data:List[str] = []
+students:List[Student] = []
+student: Student =  None
+
+try:
+    with open(fileFullPath,encoding="utf-8", mode="r") as file:
+        for line in file:
+            oneLine = line.strip() #Antalfalvai Martin  3,53
+            data = oneLine.split('\t') #tabulator kiszedese
+
+            #data[0] = Antalfalvai Martin
+            #data[1] = 3,53
+
+            student = Student()
+            student.name = data[0]
+            student.avarage = float(data[1].replace(',', '.'))
+
+            students.append(oneLine)
+except FileNotFoundError as ex:
+    print(f"{ex.filename} nem található!")
+
+for line in students:
+    print(f"{line}")
